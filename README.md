14ff64bd596f2a18c5675d86a0a879d013caad5e
jressel01 <70316907+jressel01@users.noreply.github.com>
2022-02-17 11:34:54 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Seat Connect card
+Home Assistant Lovelace UI card for Seat Connect integration
+
+This card creates a pretty (up for discussion) card for your Seat car status.
+
+## Configuration
+
+### General
+
+| Name | Type | Required | Default | Description
+| ---- | ---- | -------- | ------- | -----------
+| type | string | True | - | Must be "custom:seat-card"
+| title | string | False | - | Title of the card
+
+### Device
+| Name | Type | Required | Default | Description
+| ---- | ---- | -------- | ------- | -----------
+| device | string | True | - | The name of the device
+
+Choose the device corresponding to the vehicle you want to display.
+The card assumes that the default sensor names are used and might not work if the entities have been renamed.
+
+### Sample
+
+```yaml
+type: 'custom:seat-card'
+title: My car
+device: Leon
+```
---
+++ hass-seat-card.js
@@ -0,0 +1,231 @@
+import {
+  LitElement,
+  html,
+  css
+} from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+
+function loadCSS(url) {
+  const link = document.createElement("link");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+loadCSS("https://fonts.googleapis.com/css?family=Gloria+Hallelujah");
+
+class SeatCard extends LitElement {
+  constructor() {
+    super();
+    this.imageurl = "";
+    this.config = {};
+  }
+
+  static get styles() {
+    return style;
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    let updated = false;
+  }
+
+  static get properties() {
+    return {
+      hass: { type: Object },
+      config: { type: Object }
+    };
+  }
+
+  setConfig(config) {
+    if (!config.device) {
+      throw new Error('Please define a device (vehicle name)');
+    }
+    if (!config.entities) {
+      throw new Error('Please define a list of entities');
+    }
+    this.config = config;
+    this.imageurl = 'https://ip-modcwp.azureedge.net//modcwp3v5b20200903/0F0F-2ovtqWUlebk-6kVu2o9bdY-17NgmyXQTv.qVlH-yIC8G.6aPQ2EXMUzv-s1BZqeSWgmRyK69.kcuHUNDxO-DiSFcTLXOgdPZlAGIn-1080570studiovbeauty_connectview101281.png';
+  }
+
+  render(){
+    return html`
+      <ha-card>
+        ${this.renderContainer()}
+      </ha-card>
+    `;
+  }
+
+  renderContainer() {
+    return html `
+      <div class="seat-container">
+        ${this.renderBackground()} ${this.renderHeader()} ${this.renderStates()}
+      </div>
+    `
+  }
+
+  renderHeader() {
+    return html `
+      ${this.config.title == null || this.config.title == true
+      ? html`
+        <div class="seat-header">
+          <div class="name">
+            ${this.header}
+          </div>
+        </div>`
+      : "" }
+    `;
+  }
+
+  renderBackground() {
+    return html `
+      <img class="seat-model" src="${this.imageurl}">
+    `
+  }
+
+  // stateObj props:
+  //   entity-id
+  //   state
+  //   attributes
+  //    model
+  //    unit_of_measurement
+  //    friendly_name
+  //    icon
+  //    assumed_state
+  //   last_changed
+  //   last_updated
+  //   context
+  renderStates() {
+    return html `
+      <div class="seat-footer">
+      ${this.config.entities.map(ent => {
+        const stateObj = this._hass.states[ent];
+        return stateObj
+          ? this.renderEntity(stateObj)
+          : html`
+              <div class="not-found">Entity ${ent} not found.</div>
+            `;
+      })}
+      </div>
+    `
+  }
+
+  renderEntity(entity) {
+    var iconstate = "off";
+    if (entity.state === "on") {
+      iconstate = "on";
+    }
+    return html`
+      <div class="seat-state">
+        <div class="seat-state-icon">
+          ${this.renderIcon(entity)}
+          <span class="tooltip">${entity.attributes.friendly_name}</span>
+        </div>
+        ${this.config.states == true
+          ? html `
+            <div class="seat-state-text">
+              ${entity.state} ${entity.attributes.unit_of_measurement || ""}
+            </div>`
+          : ""
+        }
+      </div>
+    `;
+  }
+
+
+  //.overrideIcon="${config.icon === true ? entity.attributes.icon || null : config.icon}"
+  //.stateColor="${config.state_color}"
+  renderIcon(entity) {
+    return html`<state-badge
+      class="icon-small"
+      .stateObj="${entity}"
+      .stateColor="true"
+    ></state-badge>`;
+  }
+
+  // @TODO: This requires more intelligent logic
+  getCardSize() {
+    return 3;
+  }
+
+  getIcon(entity) {
+    return (
+      this.config.icon || entity.attributes.icon
+    );
+  }
+
+  static get styles() {
+    return css`
+      .seat-container {
+        position: relative;
+      }
+
+      .seat-model {
+        display: block;
+        max-width: 100%;
+        padding-top: 30px;
+        padding-bottom: 20px;
+      }
+
+      .seat-header {
+        position: absolute;
+        top: 0;
+        width: 100%;
+        padding-top: 10px;
+        text-align: center;
+        font-size: 24px;
+      }
+
+      .seat-state {
+        height: 100%;
+        width: 50px;
+        margin: 0px 5px;
+      }
+
+      .seat-state-icon {
+        position: relative;
+        display: inline-block;
+        color: var(--paper-item-icon-color, #44739e);
+        max-width: 100%;
+        margin: 0px;
+        padding: 3px 0px 2px;
+      }
+      .seat-state-icon .tooltip {
+        visibility: hidden;
+        width: 120px;
+        background-color: var(--ha-picture-card-background-color, rgba(0, 0, 0, 0.9));
+        color: #fff;
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 6px;
+
+        /* Position the tooltip text - see examples below! */
+        position: absolute;
+        z-index: 1;
+      }
+      .seat-state-icon:hover .tooltip {
+        visibility: visible;
+      }
+
+      p.seat-state-text {
+        margin: 5px 0px;
+        padding: 0px;
+      }
+
+      .seat-footer {
+        position: absolute;
+        bottom: 0px;
+        display: flex;
+        flex-wrap: wrap;
+        text-align: center;
+        justify-content: space-evenly;
+        width: 100%;
+        height: 50px;
+        background-color: var(--primary-background-color);
+        opacity: 60%;
+      }
+    `;
+  }
+}
+
+customElements.define('seat-card', SeatCard);
